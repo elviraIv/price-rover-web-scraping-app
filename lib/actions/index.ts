21012161ee07090bc427cd8e9f5f036e52d86f5d
Elvira <elvira.n.ivanova@gmail.com>
2023-11-13 17:20:07 +0200

    Update

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,15 +1,21 @@
 "use server"
 
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 
 export async function scrapeAndStoreProduct(productUrl:string) {
    if (!productUrl) return;
 
     try {
+        connectToDB();
+
         const scrapedProduct = await scrapeAmazonProduct(productUrl)
+        
         if(!scrapedProduct) return;
 
-        
+
+        let product = scrapedProduct;
+        // const exisistingProduct = await Product
         
     } catch (error:any) {
         throw new Error(`Failed to create/update product:${error.message}`)
