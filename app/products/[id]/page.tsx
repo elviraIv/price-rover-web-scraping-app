b6ce547b0768b106cebbbb1f35bde30b3a28e745
Elvira <elvira.n.ivanova@gmail.com>
2023-11-14 18:07:41 +0200

    Add Modal to details pages

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/Modal";
 import PriceInfoCard from "@/components/PriceInfoCard";
 import ProductCard from "@/components/ProductCard";
 import { getProductById, getSimilarProducts } from "@/lib/actions";
@@ -157,7 +158,9 @@ const ProductDetails = async ({ params: { id } }: Props) => {
               />
             </div>
           </div>
-          Modal
+
+          <Modal productId={id}/>
+
         </div>
       </div>
 
